036d0aa18ed4d3d9b2a6cb9083e308f96bb7065e
gh662 <a25@mail.ru>
2022-06-23 20:17:39 +0300

    Первый коммит

---
+++ app/src/main/java/ru/desk/testgit2/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.desk.testgit2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
