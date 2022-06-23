3ea03c999ae2ad8aca72e04510f34d4cfb533385
gh662 <a25@mail.ru>
2022-06-23 20:18:48 +0300

    //первое обновиление тестирование коммита 2

--- app/src/main/java/ru/desk/testgit2/MainActivity.kt
+++ app/src/main/java/ru/desk/testgit2/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //первое обновиление тестирование коммита 2
     }
 }
